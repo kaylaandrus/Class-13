89a1bdb11aa59b24b3565276c939d39a72dd6113
kaylaandrus <kaylam.andrus@gmail.com>
2022-03-28 14:45:49 -0500

    Class 13 Exercises

---
+++ basic-event-binding-exercise/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'basic-event-binding-exercise';
+  createLog() {
+    return console.log('You have been logged');
+  }
+}
